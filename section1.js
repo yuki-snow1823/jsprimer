0417d7fa5e84baf7517646277731a06aa060aa0a
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-07 14:43:35 +0900

    [section1]ループと反復処理まで

--- section1.js
+++ section1.js
@@ -151,8 +151,6 @@ const obj = {
   }
 };
 console.log(obj.method()); // => "this is method"
-
-
 // 式と文
 
 // 関数式をread変数へ代入
@@ -163,3 +161,27 @@ if (true) {
   console.log("文1");
   console.log("文2");
 }
+
+
+// 条件分岐
+if (true) {
+  console.log("この行は実行されます");
+}
+
+// rubyはelsif、JSはelse if
+function getECMAScriptName(version) {
+  switch (version) {
+    case "ES5":
+      return "ECMAScript 5";
+    case "ES6":
+      return "ECMAScript 2015";
+    case "ES7":
+      return "ECMAScript 2016";
+    default:
+      return "しらないバージョンです";
+  }
+}
+// 関数を実行して`return`された値を得る
+getECMAScriptName("ES6"); // => "ECMAScript 2015"
+
+// break文の多用があるなら、書き換えるべき方法を考える
