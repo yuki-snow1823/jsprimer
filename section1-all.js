5523bd98410eb3b54aa5a9825b66b6ea96a269d1
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-27 17:37:58 +0900

    [add]async await

--- section1-all.js
+++ section1-all.js
@@ -681,3 +681,26 @@ promise.then(
 );
 
 // Async Function
+
+// 通常の関数でPromiseインスタンスを返している
+function doAsync() {
+    return Promise.resolve("値");
+}
+doAsync().then(value => {
+    console.log(value); // => "値"
+});
+
+async function asyncMain() {
+  // await式のエラーはtry...catchできる
+  try {
+    // `await`式で評価した右辺のPromiseがRejectedとなったため、例外がthrowされる
+    const value = await Promise.reject(new Error("エラーメッセージ"));
+    // await式で例外が発生したため、この行は実行されません
+  } catch (error) {
+    console.log(error.message); // => "エラーメッセージ"
+  }
+}
+// asyncMainはResolvedなPromiseを返す
+asyncMain().catch((error) => {
+  // すでにtry...catchされているため、この行は実行されません
+});
